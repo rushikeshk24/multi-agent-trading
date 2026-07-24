4652b43df13ec0df7f32469f7600b2d4bd4d808b
rushikeshk24 <rushikeshkarlekar14@gmail.com>
2026-07-24 23:28:39 +0530

    new frontend changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://multi-agent-trading.onrender.com",
 });
 
 export const analyzeStock = (symbol) =>
